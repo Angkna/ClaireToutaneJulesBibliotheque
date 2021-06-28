e6898ba884cf7231597ba3d459fe2d7b464e955a
JulesFlores <JulesFlores@ldnr.fr>
2021-06-28 11:23:08 +0200

    Add class livre

---
+++ src/main/java/fr/ldnr/clairetoutanejulesbibliotheque/Livre.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ldnr.clairetoutanejulesbibliotheque;
+
+/**
+ *
+ * @author stag
+ */
+public class Livre {
+    
+}
