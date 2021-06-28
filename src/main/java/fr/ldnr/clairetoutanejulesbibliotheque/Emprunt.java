76203042dbfb3285518df7c72ba86e9c019e4731
Toutane Coly <toutane.coly@gmail.com>
2021-06-28 13:39:42 +0200

    Ajout du bean Emprunt créé et annoté.

---
+++ src/main/java/fr/ldnr/clairetoutanejulesbibliotheque/Emprunt.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ldnr.clairetoutanejulesbibliotheque;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author stag
+ */
+@Entity
+@Table (name="Emprunts")
+public class Emprunt implements Serializable {
+    private static final long serialVersionUID=1L;
+   private int id;
+    private Livre livre;
+    private String nomUser;
+    private Date dateEmprunt;
+    private Date dateRendu;
+
+    public Emprunt() {
+        
+    }
+@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+@Column 
+    public Livre getLivre() {
+        return livre;
+    }
+
+    public void setLivre(Livre livre) {
+        this.livre = livre;
+    }
+@Column (length=100, nullable = false)
+    public String getNomUser() {
+        return nomUser;
+    }
+
+    public void setNomUser(String nomUser) {
+        this.nomUser = nomUser;
+    }
+@Column
+    public Date getDateEmprunt() {
+        return dateEmprunt;
+    }
+
+    public void setDateEmprunt(Date dateEmprunt) {
+        this.dateEmprunt = dateEmprunt;
+    }
+@Column
+    public Date getDateRendu() {
+        return dateRendu;
+    }
+
+    public void setDateRendu(Date dateRendu) {
+        this.dateRendu = dateRendu;
+    }
+    
+}
+
