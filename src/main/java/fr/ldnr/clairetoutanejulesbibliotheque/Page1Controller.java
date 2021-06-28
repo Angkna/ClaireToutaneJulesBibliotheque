8117b2ceeb292448bae9d1ddc46747f9c234b052
Claire De Giovanni <cc.degiovanni@gmail.com>
2021-06-28 14:31:07 +0200

    Ajout Page1Controller.java

---
+++ src/main/java/fr/ldnr/clairetoutanejulesbibliotheque/Page1Controller.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ldnr.clairetoutanejulesbibliotheque;
+
+/**
+ * @author Claire
+ * @version 28/June/2021 p.m.
+ */
+
+@RestController
+@RequestMapping("/index")
+public class Page1Controller {
+    
+}
