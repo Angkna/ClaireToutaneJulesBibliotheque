2510c3242949940a3be7959a6e80f67d4a429180
Toutane Coly <toutane.coly@gmail.com>
2021-06-28 14:13:05 +0200

    lien entre bean et annotation fait dans HibernateConfi.java

--- src/main/java/fr/ldnr/clairetoutanejulesbibliotheque/HibernateConfig.java
+++ src/main/java/fr/ldnr/clairetoutanejulesbibliotheque/HibernateConfig.java
@@ -14,28 +14,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HibernateConfig {
 
-	//On recree la configuration en java (a la place du fichier xml : hibernate.cfg.xml)
-	@Bean
-	public SessionFactory sessionFactory() {
-		
-		Properties options = new Properties();
-		//<property name="hibernate.connection.driver_class">org.sqlite.JDBC</property>
-		options.put("hibernate.connection.driver_class", "org.sqlite.JDBC");
-		//<property name="hibernate.connection.url">jdbc:sqlite:nom.sqlite</property>
-		options.put("hibernate.connection.url", "jdbc:sqlite:bibliothequeData.sqlite");
-		//<property name="hibernate.dialect">org.sqlite.hibernate.dialect.SQLiteDialect</property>
-		options.put("hibernate.dialect", "org.sqlite.hibernate.dialect.SQLiteDialect");
-		//<property name="hibernate.show_sql">true</property>
-		options.put("hibernate.show_sql", "true");
-        //<property name="hibernate.hbm2ddl.auto">create</property>
-		options.put("hibernate.hbm2ddl.auto", "update");
-		
-		SessionFactory factory = new org.hibernate.cfg.Configuration()
-				.addProperties(options)
-				//<mapping class="fr.ldnr.julesflores.mavenproject4.Inscription"/> 
-				.addAnnotatedClass(Livre.class)
-				.buildSessionFactory();
-		return factory;
-	}
-}
+    //On recree la configuration en java (a la place du fichier xml : hibernate.cfg.xml)
+    @Bean
+    public SessionFactory sessionFactory() {
 
+        Properties options = new Properties();
+        //<property name="hibernate.connection.driver_class">org.sqlite.JDBC</property>
+        options.put("hibernate.connection.driver_class", "org.sqlite.JDBC");
+        //<property name="hibernate.connection.url">jdbc:sqlite:nom.sqlite</property>
+        options.put("hibernate.connection.url", "jdbc:sqlite:bibliothequeData.sqlite");
+        //<property name="hibernate.dialect">org.sqlite.hibernate.dialect.SQLiteDialect</property>
+        options.put("hibernate.dialect", "org.sqlite.hibernate.dialect.SQLiteDialect");
+        //<property name="hibernate.show_sql">true</property>
+        options.put("hibernate.show_sql", "true");
+        //<property name="hibernate.hbm2ddl.auto">create</property>
+        options.put("hibernate.hbm2ddl.auto", "update");
+
+        SessionFactory factory = new org.hibernate.cfg.Configuration()
+                .addProperties(options)
+                //<mapping class="fr.ldnr.julesflores.mavenproject4.Inscription"/> 
+                .addAnnotatedClass(Livre.class)
+                .addAnnotatedClass(Emprunt.class)
+                .buildSessionFactory();
+        return factory;
+    }
+}
