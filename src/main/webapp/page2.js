68078406335128f4301dc9d1683cddbb2a8c555d
Toutane Coly <toutane.coly@gmail.com>
2021-06-29 11:53:33 +0200

    ajout du tableau de tests

--- src/main/webapp/page2.js
+++ src/main/webapp/page2.js
@@ -1,28 +1,28 @@
-$(function() {
-	// sélectionner l'élément d'id "envoyer"
-	$("#enregistrer").on('click', function(){
-		$("#message").html("Enregistrer !");
-		let titreLivre = $("#titreLivre").val();
-		let dateEmprunt= $("#dateEmprunt").val();
-                let nomEmprunteur=$("#nomEmprunt").val();
-		
+$(function () {
+    // sélectionner l'élément d'id "envoyer"
+    $("#enregistrer").on('click', function () {
+        $("#message").html("Enregistrer !");
+        let titreLivre = $("#titreLivre").val();
+        let dateEmprunt = $("#dateEmprunt").val();
+        let nomEmprunteur = $("#nomEmprunt").val();
+
 //		Envoyer les données vers le serveur à l'addresse /page2 et mettre les données dans  la requête
-		$.ajax({
-			url:"/page2",
-			type:"POST",
-			contentType:"application/json",
-			data:JSON.stringify({
-				"titreLivre":titreLivre,
-				"dateEmprunt":dateEmprunt,
-				"nomEmprunteur":nomEmprunteur
-                                
-			})
-		}).done(function(retour){ // 200
+        $.ajax({
+            url: "/page2",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({
+                "titreLivre": titreLivre,
+                "dateEmprunt": dateEmprunt,
+                "nomEmprunteur": nomEmprunteur
 
-			$("#message").html(retour);
+            })
+        }).done(function (retour) { // 200
 
-		}).fail(function(){ // 400, 501..
-			$("#message").html("Echec !");
-		});
-	});
+            $("#message").html(retour);
+
+        }).fail(function () { // 400, 501..
+            $("#message").html("Echec !");
+        });
+    });
 });
