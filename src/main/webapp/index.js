619755dba6036fd7adc5deca7d1d23291d0a2910
JulesFlores <JulesFlores@ldnr.fr>
2021-06-29 09:42:25 +0200

    update page1 fonctionnement

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -7,17 +7,17 @@
 $(function() {
 	// sélectionner l'élément d'id "envoyer"
 	$("#envoyer").on('click', function(){
-		$("#message").html("Envoi !")
-		let titre = $("#titre").val()
-		let annee = $("#annee").val()
-		let editeur = $("#editeur").val()
-		let nomAuteur = $("#nomA").val()
-                let prenomAuteur = $("#prenomA").val()
+		$("#message").html("Envoi !");
+		let titre = $("#titre").val();
+		let annee = $("#annee").val();
+		let editeur = $("#editeur").val();
+		let nomAuteur = $("#nomA").val();
+                let prenomAuteur = $("#prenomA").val();
 //		Envoyer les données vers le serveur à l'addresse /envoi et en mettant toutes ces données-là dans le coeur de la requête
 		$.ajax({
 			url:"/index/envoi",
 			type:"POST",
-			dataType:"json",
+			//dataType:"json",
 			contentType:"application/json",
 			data:JSON.stringify({
 				"titre":titre,
@@ -27,9 +27,9 @@ $(function() {
                                 "prenomAuteur":prenomAuteur
 			})
 		}).done(function(retour){ // 200
-			$("#message").html("Reçu !")
+			$("#message").html(retour);
 		}).fail(function(){ // 400, 501..
-			$("#message").html("Echec !")
-		})
-	})
-})
+			$("#message").html("Echec !");
+		});
+	});
+});
