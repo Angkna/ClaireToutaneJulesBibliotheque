f71faa89dd80df7ecefa201641e7e25cc02eddf0
JulesFlores <JulesFlores@ldnr.fr>
2021-06-29 15:26:15 +0200

    Emprunt persistance ok

--- src/main/java/fr/ldnr/clairetoutanejulesbibliotheque/Page2Controller.java
+++ src/main/java/fr/ldnr/clairetoutanejulesbibliotheque/Page2Controller.java
@@ -5,12 +5,14 @@
  */
 package fr.ldnr.clairetoutanejulesbibliotheque;
 
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +30,7 @@ public class Page2Controller {
     public static final Logger logger = LoggerFactory.getLogger(Page2Controller.class);
 
     public SessionFactory sessionFactory;
+
     // Permet de créer une SessionFactory avec les configurations souhaitées automatiquement
     @Autowired
     public void setSessionFactory(SessionFactory sessionFactory) {
@@ -35,13 +38,41 @@ public class Page2Controller {
     }
 
     @RequestMapping(value = "/envoi", method = RequestMethod.POST)
-    public String envoi(@RequestBody Emprunt emprunt) {
-        logger.info("" + emprunt.getNomUser() + " " + emprunt.getDateEmprunt());
+    public String envoi(@RequestBody LightEmprunt lightEmprunt) {
+        String message;
+        logger.info("titre " + lightEmprunt.getTitreLivre() + " " +lightEmprunt.getNomUser() + " " + lightEmprunt.getDateEmprunt() );
         Session session = sessionFactory.openSession();
         Transaction tx = session.beginTransaction();
-        session.save(emprunt);
-        tx.commit();
+        try {
+            String insHQL = "from Livre where titre = :t";
+            @SuppressWarnings("unchecked")
+            List<Livre> livres = session.createQuery(insHQL).setParameter("t", lightEmprunt.getTitreLivre()).getResultList();
+            Emprunt emprunt = new Emprunt();
+            emprunt.setDateEmprunt(lightEmprunt.getDateEmprunt());
+            emprunt.setNomUser(lightEmprunt.getNomUser());
+            emprunt.setLivre(livres.get(0));
+            session.save(emprunt);
+            tx.commit();
+            logger.info("emprunt crée du livre : " + emprunt.getLivre().toString() );
+            message = "Reçu !";
+        } catch (Exception e) {
+            tx.rollback();
+            logger.warn("Rollback!" + e.getMessage());
+            message = "Fail !";
+        }
         session.close();
-        return "Reçu !";
+        return message;
     }
+
+    @RequestMapping(value = "/{titre}", method = RequestMethod.GET)
+    public List<Livre> lire(@PathVariable String titre)    {
+        logger.info("Recherche livres avec comme titre : " + titre);
+        Session ses = sessionFactory.openSession();
+        String insHQL = "from Livre where titre like :t";
+        @SuppressWarnings("unchecked")
+        List<Livre> livres = ses.createQuery(insHQL).setParameter("t", titre + "%").getResultList();
+        ses.close();
+        return livres;
+    }
+
 }
--- src/main/webapp/page2.js
+++ src/main/webapp/page2.js
@@ -1,23 +1,17 @@
-$(function() {
-	// sélectionner l'élément d'id "envoyer"
-	$("#enregistrer").on('click', function(){
-		$("#message").html("Enregistrer !");
-//		let titreLivre = $("#titreLivre").val();
-		let dateEmprunt = $("#dateEmprunt").val();
-                let nomEmprunteur=$("#nomEmprunt").val();
-//		Envoyer les données vers le serveur à l'addresse /page2 et mettre les données dans  la requête
+$(function () {
+    // sélectionner l'élément d'id "envoyer"
+    $("#enregistrer").on('click', function () {
+        $("#message").html("Enregistrer !");
+	let titreLivre = $("#titreLivre").val();
+        let dateEmprunt = $("#dateEmprunt").val();
+        let nomEmprunteur = $("#nomEmprunteur").val();
+//	Envoyer les données vers le serveur à l'addresse /page2 et mettre les données dans  la requête
         $.ajax({
             url: "/page2/envoi",
             type: "POST",
             contentType: "application/json",
             data: JSON.stringify({
-                "livre": JSON.stringify({
-                    "titre": titre,
-                    "annee": annee,
-                    "editeur": editeur,
-                    "nomAuteur": nomAuteur,
-                    "prenomAuteur": prenomAuteur
-                }),
+                "titreLivre" : titreLivre,
                 "dateEmprunt": dateEmprunt,
                 "nomUser": nomEmprunteur
             })
@@ -26,5 +20,20 @@ $(function() {
         }).fail(function () { // 400, 501..
             $("#message").html("Echec !");
         });
+
+    });
+
+    $("#titreLivre").on('input', function () {
+        let titre = $("#titreLivre").val();
+        $.ajax({
+            url: "/page2/" + titre,
+            type: "GET",
+            dataType: "json"
+        }).done(function (listRetour) { //200
+            $("#message").html("Trouvé ! Livres ayant le titre de " + titre + " (" + listRetour.length + ").");
+        }).fail(function () { //400, 501...
+            $("#message").html("Serveur non disponible !");
+        });
+
     });
 });
