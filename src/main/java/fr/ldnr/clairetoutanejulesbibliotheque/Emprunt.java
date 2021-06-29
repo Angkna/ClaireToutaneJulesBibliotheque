8cfbbcc93fb1167f7789b350bd6aa34f4f2c3ec5
Claire De Giovanni <cc.degiovanni@gmail.com>
2021-06-29 11:10:44 +0200

    Ajout bouton vers page 2 sur index.html + annotation @ManyToOne sur Emprunt.java

--- src/main/java/fr/ldnr/clairetoutanejulesbibliotheque/Emprunt.java
+++ src/main/java/fr/ldnr/clairetoutanejulesbibliotheque/Emprunt.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 
@@ -41,6 +42,7 @@ public class Emprunt implements Serializable {
         this.id = id;
     }
 @Column 
+@ManyToOne
     public Livre getLivre() {
         return livre;
     }
